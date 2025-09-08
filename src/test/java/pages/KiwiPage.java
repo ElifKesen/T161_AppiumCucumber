7ad6f29d95fd6175c71b42157b3e94105688a7f9
Elif Kesen <elifkesen@gmail.com>
2025-09-08 21:49:02 +0200

    v1

--- src/test/java/pages/KiwiPage.java
+++ src/test/java/pages/KiwiPage.java
@@ -17,6 +17,54 @@ public class KiwiPage {
     @FindBy(xpath = "//*[@text='Continue as a guest']")
     public WebElement ContinueAsButton;
 
+    @FindBy(xpath = "//*[@text='One way']")
+    public WebElement OneWayButton;
+
+    @FindBy(xpath = " (//*[@class='android.view.View'])[5]")
+    public WebElement secimButton;
+
+    @FindBy(xpath = "//*[@text='From:']")
+    public WebElement FromButton;
+
+    @FindBy(xpath = "//*[@content-desc='Clear All']")
+    public WebElement ClearButton;
+
+    @FindBy(xpath = "//*[@class='android.widget.EditText']")
+    public WebElement KalkisVarisYeriYazmaKutusu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.Button'])[2]")
+    public WebElement sehirSecmeButonu;
+
+    @FindBy(xpath = "//*[@text='Choose']")
+    public WebElement ChooseButonu;
+
+    @FindBy(xpath = "//*[@text='To:']")
+    public WebElement ToButonu;
+
+    @FindBy(xpath = "//*[@text='Departure:']")
+    public WebElement DepartureButonu;
+
+    @FindBy(xpath="//*[@text='Set date']")
+    public WebElement setDatebutonu;
+
+    @FindBy(xpath="/(//*[@text='Search'])[1]")
+    public WebElement searchbutonu;
+
+    @FindBy(xpath="//*[@text='Best']")
+    public WebElement bestbutonu;
+
+    @FindBy(xpath="//*[@text='Cheapest']")
+    public WebElement enUcuzbutonu;
+
+    @FindBy(xpath="//*[@text='Stops']")
+    public WebElement Stopbutonu;
+
+    @FindBy(xpath="//*[@text='Nonstop']")
+    public WebElement nonStopbutonu;
+
+    @FindBy(xpath = "(//*[@class='android.widget.TextView'])[11]")
+    public  WebElement fiyatBilgisi;
+
 
 
 }
--- src/test/java/stepDefinitions/KiwiStepDef.java
+++ src/test/java/stepDefinitions/KiwiStepDef.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pages.KiwiPage;
+import utilities.Driver;
 import utilities.ResuableMethods;
 
 public class KiwiStepDef {
@@ -37,36 +38,74 @@ public class KiwiStepDef {
 
 
 
-
     @When("Trip type one way olarak secilir")
-    public void trip_type_one_way_olarak_secilir() {
+    public void trip_type_one_way_olarak_secilir() throws InterruptedException {
+
+        Thread.sleep(3000);
+        page.secimButton.click();
+        Thread.sleep(3000);
+        page.OneWayButton.click();
 
     }
 
     @When("Kalkıs sehri secenegine tiklanir ve default olan sehir kaldirilir")
-    public void kalkıs_sehri_secenegine_tiklanir_ve_default_olan_sehir_kaldirilir() {
-
+    public void kalkıs_sehri_secenegine_tiklanir_ve_default_olan_sehir_kaldirilir() throws InterruptedException {
+        Thread.sleep(3000);
+        page.FromButton.click();
+        page.ClearButton.click();
     }
 
     @When("Kalkıs sehri alanina Ankara yazilir ve Choose butonuna tiklanir")
-    public void kalkıs_sehri_alanina_ankara_yazilir_ve_choose_butonuna_tiklanir() {
+    public void kalkıs_sehri_alanina_ankara_yazilir_ve_choose_butonuna_tiklanir() throws InterruptedException {
+        Thread.sleep(3000);
+        page.KalkisVarisYeriYazmaKutusu.click();
 
+        if (Driver.getAndroidDriver().isKeyboardShown()){
+            Driver.getAndroidDriver().getKeyboard().pressKey("Ankara");
+        }else {
+            page.KalkisVarisYeriYazmaKutusu.sendKeys("Ankara");
+        }
+        Thread.sleep(3000);
+        page.sehirSecmeButonu.click();
+        page.ChooseButonu.click();
     }
 
     @When("Varış sehri alanina Frankfurt yazilir ve Choose butonuna tiklanir")
-    public void varış_sehri_alanina_frankfurt_yazilir_ve_choose_butonuna_tiklanir() {
+    public void varış_sehri_alanina_frankfurt_yazilir_ve_choose_butonuna_tiklanir() throws InterruptedException {
+        page.ToButonu.click();
+        page.KalkisVarisYeriYazmaKutusu.click();
+        Thread.sleep(3000);
+        if (Driver.getAndroidDriver().isKeyboardShown()){
+            Driver.getAndroidDriver().getKeyboard().pressKey("Frankfurt");
+        }else {
+            page.KalkisVarisYeriYazmaKutusu.sendKeys("Frankfurt");
+        }
+        Thread.sleep(3000);
+        page.sehirSecmeButonu.click();
+        page.ChooseButonu.click();
 
     }
     @When("Gidiş tarihi alanindan {int} Eylul secilir ve Set Date butonuna tiklanir")
-    public void gidiş_tarihi_alanindan_eylul_secilir_ve_set_date_butonuna_tiklanir(Integer int1) {
+    public void gidiş_tarihi_alanindan_eylul_secilir_ve_set_date_butonuna_tiklanir(Integer int1) throws InterruptedException {
+        page.DepartureButonu.click();
+        Thread.sleep(3000);
+        ResuableMethods.koordinatTiklamaMethodu(392,1245,300);
+        Thread.sleep(3000);
+        page.setDatebutonu.click();
 
     }
     @When("Search butonuna tiklanir")
-    public void search_butonuna_tiklanir() {
+    public void search_butonuna_tiklanir() throws InterruptedException {
+        Thread.sleep(3000);
+        page.searchbutonu.click();
 
     }
     @When("En ucuz ve Aktarmasiz filtrelemeleri yapilir")
     public void en_ucuz_ve_aktarmasiz_filtrelemeleri_yapilir() {
+        page.bestbutonu.click();
+        page.enUcuzbutonu.click();
+        page.Stopbutonu.click();
+        page.nonStopbutonu.click();
 
     }
     @Then("Gelen ilk bilet fiyati kaydedilir ve kullanicinin telefonuna SMS olarak gonderilir")
